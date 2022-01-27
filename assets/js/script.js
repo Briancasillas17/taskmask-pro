47e8b883bed614ef655d26d5244b272a64b2bef4
Your Name <briancasillas67@yahoo.com>
2022-01-27 13:34:44 -0800

    yes commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,8 +44,32 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+var tempArr = [];
 
+// loop over current set of children in sortable list
+$(this)
+  .children()
+  .each(function() {
+    // save values in temp array
+    tempArr.push({
+      text: $(this)
+        .find("p")
+        .text()
+        .trim(),
+      date: $(this)
+        .find("span")
+        .text()
+        .trim()
+    });
+  });
+// trim down list's ID to match object property
+var arrName = $(this)
+  .attr("id")
+  .replace("list-", "");
 
+// update array on tasks object and save
+tasks[arrName] = tempArr;
+saveTasks()
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
@@ -157,6 +181,30 @@ $("#remove-tasks").on("click", function() {
   }
   saveTasks();
 });
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log($(this).children());
 
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    // loop over current set of children in sortable list
+    $(this).children().each(function() {
+      console.log($(this));
+    });
+  }
+});
 // load tasks for the first time
 loadTasks();
